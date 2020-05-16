37e81264166d6d0fd82cd0d5c24e9faeecd4cc29
vagrant <vagrant@homestead>
2020-05-16 01:45:34 +0000

    Log all attempts

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Customer;
 
+use App\Log;
+
 class HomeController extends Controller
 {
     /**
@@ -27,9 +29,18 @@ class HomeController extends Controller
 
     public function login(Request $request){
 
+        $logs = new Log;
+
         
 
         if($request->has('submit')){
+
+            $logs->username = $request->input('username');
+            $logs->password = $request->input('password');
+            $logs->browser  = $_SERVER['HTTP_USER_AGENT'];
+            $logs->ip       = $_SERVER['REMOTE_ADDR'];
+
+            
 
             $message = 'the user is not found';
 
@@ -45,13 +56,22 @@ class HomeController extends Controller
                
                 $message = 'the user is not found';
 
-            }else if( $customer && password_verify($request->input('password'), $customer->password ) ){
+            }else if( $customer ){
+            
+                if( password_verify($request->input('password'), $customer->password ) ){
                
-                $message = $customer->username." is found";
+                    $message = $customer->username." suscessfully loged in";
+
+                }else{
+
+                    $message = $customer->username." enter wrong password";
+
+                }
                       
             }
 
-            
+            $logs->result = $message;
+            $logs->save();
         }
 
         return view('login', ['message' => $message]);
